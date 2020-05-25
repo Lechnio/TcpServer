b0619770b413fbedb03c35fe1b96993a26797116
Lechnio <kuba.fr@wp.pl>
2020-05-25 03:18:48 +0200

    fix: turn off -Wpedantic, move dependency, fix no parameter log macro

--- src/log.h
+++ src/log.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef DEBUG
-#   define dlog(msg, ...) printf("D: " msg "\n", __VA_ARGS__)
+#   define dlog(msg, ...) printf("D: " msg "\n", ## __VA_ARGS__)
 #else
 #   define dlog(msg, ...)
 #endif
